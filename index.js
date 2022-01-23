045223ae0aa3771ca84d87b80f1fb639901921b7
rpaezbas <rpaezbas@rpaezbas.com>
2022-01-23 19:27:30 +0100

    contacts persisted

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const { appendChatMessage, textarea } = require('./lib/gui')
+const gui = require('./lib/gui')
 const command = require('./lib/command')
+const configuration = require('./lib/config')
 
-appendChatMessage(Date.now(), 'userA', 'In id erat non orci commodo lobortis.  ')
-appendChatMessage(Date.now(), 'userB', 'Donec vitae dolor.  ')
-appendChatMessage(Date.now(), 'userA', 'Etiam laoreet quam sed arcu.  ')
-appendChatMessage(Date.now(), 'userB', 'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  ')
-appendChatMessage(Date.now(), 'userA', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  ')
+// TODO remove lorem ipsum
+// TODO add date format
+gui.appendChatMessage(Date.now(), 'userA', 'In id erat non orci commodo lobortis.  ')
+gui.appendChatMessage(Date.now(), 'userB', 'Donec vitae dolor.  ')
+gui.appendChatMessage(Date.now(), 'userA', 'Etiam laoreet quam sed arcu.  ')
+gui.appendChatMessage(Date.now(), 'userB', 'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  ')
+gui.appendChatMessage(Date.now(), 'userA', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  ')
 
-textarea.on('submit', (data) => {
-  const isCommand = data[0] === '\\'
-  if (isCommand) {
-    command(data.substring(1).split(' '))
-  } else {
-    // sendMessageToRelay
-  }
-})
+const run = async () => {
+  (await configuration.loadConfig()).contacts.filter(c => c.active).forEach(c => {
+    gui.addContact(c.alias)
+  })
+
+  gui.textarea.on('submit', (data) => {
+    const isCommand = data[0] === '\\'
+    if (isCommand) {
+      command(data.substring(1).split(' '))
+    } else {
+      // sendMessageToRelay
+    }
+  })
+}
+
+run()
