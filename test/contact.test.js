8659977e223d5e22c4bd928c2567a83615601e7b
rpaezbas <rpaezbas@rpaezbas.com>
2022-01-23 14:48:41 +0100

    added contacts module

---
+++ test/contact.test.js
@@ -0,0 +1,13 @@
+const test = require('brittle')
+const contact = require('../lib/contact')
+const crypto = require('../lib/crypto')
+
+test('generates new contact from masterkey', async ({ is, not }) => {
+  const masterkey = crypto.generateMasterkey()
+  const {address, seed, pk, sk } = contact.contact(masterkey, 0)
+  is(address.toString(), crypto.deriveSubkey(masterkey, [0]).toString())
+  is(seed.toString(), crypto.deriveSubkey(masterkey, [1]).toString())
+  not(pk, undefined)
+  not(sk, undefined)
+  not(pk.toString(), sk.toString())
+})
