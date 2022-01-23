cef8f06b8ad09dbde8e13648b9d3b8bd8991c865
rpaezbas <rpaezbas@rpaezbas.com>
2022-01-23 18:00:14 +0100

    added create-contact command

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { appendChatMessage, textarea } = require('./lib/gui')
+const command = require('./lib/command')
 
 appendChatMessage(Date.now(), 'userA', 'In id erat non orci commodo lobortis.  ')
 appendChatMessage(Date.now(), 'userB', 'Donec vitae dolor.  ')
@@ -7,5 +8,10 @@ appendChatMessage(Date.now(), 'userB', 'Proin quam nisl, tincidunt et, mattis eg
 appendChatMessage(Date.now(), 'userA', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  ')
 
 textarea.on('submit', (data) => {
-  appendChatMessage(data)
+  const isCommand = data[0] === '\\'
+  if (isCommand) {
+    command(data.substring(1).split(' '))
+  } else {
+    // sendMessageToRelay
+  }
 })
--- test/contact.test.js
+++ test/contact.test.js
@@ -4,7 +4,7 @@ const crypto = require('../lib/crypto')
 
 test('generates new contact from masterkey', async ({ is, not }) => {
   const masterkey = crypto.generateMasterkey()
-  const {address, seed, pk, sk } = contact.contact(masterkey, 0)
+  const { address, seed, pk, sk } = contact.contact(masterkey, 0)
   is(address.toString(), crypto.deriveSubkey(masterkey, [0]).toString())
   is(seed.toString(), crypto.deriveSubkey(masterkey, [1]).toString())
   not(pk, undefined)
