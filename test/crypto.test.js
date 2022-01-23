a5c6818d721e562ca18d5017d960c5124805559a
rpaezbas <rpaezbas@rpaezbas.com>
2022-01-23 13:36:26 +0100

    readded keypairs

--- test/crypto.test.js
+++ test/crypto.test.js
@@ -14,13 +14,39 @@ test('generates subkeys', async ({ is, not }) => {
   not(subkey.toString(), subkey1.toString())
 })
 
+test('Keypair generation works with/without seed', async ({ is, not }) => {
+  const keyPair1 = crypto.keyPair()
+  const seed = Buffer.allocUnsafe(32).toString('hex')
+  const keyPair2 = crypto.keyPair(Buffer.from(seed, 'hex'))
+  not(keyPair1.seed && keyPair1.pk && keyPair1.sk, undefined)
+  not(keyPair2.seed && keyPair2.pk && keyPair2.sk, undefined)
+  is(seed, keyPair2.seed.toString('hex'))
+})
+
 test('encrypt/decrypt message with master key', async ({ is, not }) => {
   const message = 'Hello world!'
-  const key = crypto.generateMasterkey()
-  const ciphertext = crypto.encrypt(Buffer.from(message), key)
-  const plaintext = crypto.decrypt(ciphertext, key)
+  const { pk, sk } = crypto.keyPair()
+  const ciphertext = crypto.encrypt(Buffer.from(message), pk)
+  const plaintext = crypto.decrypt(ciphertext, pk, sk)
   not(ciphertext, undefined)
   not(plaintext, undefined)
-  is(ciphertext.length, message.length + 16) // sodium.crypto_secretbox_MACBYTES
+  not(ciphertext, plaintext)
   is(plaintext.toString(), message)
+})
+
+test('can create keyPair from masterkey', async ({ is, not }) => {
+  const key = crypto.generateMasterkey().toString('hex')
+  const { seed, pk, sk } = crypto.keyPair(Buffer.from(key, 'hex'))
+  is(key, seed.toString('hex'))
+  not(pk, undefined)
+  not(sk, undefined)
+})
+
+test('creates same keypair from same masterkey', async ({ is, not }) => {
+  const key = crypto.generateMasterkey().toString('hex')
+  const { seed1, pk1, sk1 } = crypto.keyPair(Buffer.from(key, 'hex'))
+  const { seed2, pk2, sk2 } = crypto.keyPair(Buffer.from(key, 'hex'))
+  is(seed1, seed2)
+  is(pk1, pk2)
+  is(sk1, sk2)
 })
