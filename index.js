8fda350a6cdbf70050e1bf7f5a5fd5f43b91f796
rpaezbas <rpaezbas@rpaezbas.com>
2022-02-26 03:17:19 +0100

    added watch to relay client

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const RelayClient = require('./lib/relay-client')
 const Corestore = require('corestore')
 const ram = require('random-access-memory') // TODO remove
 const gui = require('./lib/gui')
-const c = require('compact-encoding')
+const { decode } = require('compact-encoding')
 const loadDB = require('./lib/db')
 
 const store = new Corestore(ram)
 const relayClients = new Map()
 const messages = new Map()
 
-console.log = gui.appendLogMessage //TODO debug only
+console.log = gui.appendLogMessage // TODO debug only
 
 const start = async () => {
   // Load configuration and render contacts
@@ -34,17 +34,28 @@ const start = async () => {
         return relayClient
       }
       messages.set(c.alias, { in: [], out: [] })
+
+      // Get inbox messages
       const inbox = await initRelayClient(c.inbox)
       await inbox.ready()
       messages.get(c.alias).in = await getMessages(inbox, c.inbox)
-      inbox.on('message', console.log) // TODO
+      const { rootAddress } = invitationTransform(c.inbox)
+      inbox.watch(rootAddress, messages.get(c.alias).in.length, c.alias)
+      inbox.on('message', m => {
+        if (m.alias === c.alias) {
+          messages.get(c.alias).in.push(decode(query, m.value))
+          //TODO if checking different chat, add * to contact
+        }
+      })
+
+      // Get outbox messages
       if (c.outbox) { // inbox is always there, but outbox might not be there yet
         const outbox = await initRelayClient(c.outbox)
         await outbox.ready()
         const range = (key) => ({ gt: key, lt: key + '~' })
-        console.log("searching messages...")
-        for await (message of db.createReadStream(range(c.alias))){
-          messages.get(c.alias).out.push({ timestamp:message.key.toString().split('!')[1] , payload: message.value }) // TODO add timestamp
+        console.log('searching messages...')
+        for await (message of db.createReadStream(range(c.alias))) {
+          messages.get(c.alias).out.push({ timestamp: message.key.toString().split('!')[1], payload: message.value })
         }
       }
     })
@@ -60,15 +71,16 @@ const start = async () => {
       const messagesNum = messages.get(currentContact.alias).out.length
       const { rootAddress, pk, signPk, relay } = invitationTransform(currentContact.outbox)
       const { signKeyPair } = await contact(Buffer.from(config.masterkey, 'hex'), gui.getSelectedContactIndex())
-      const message = { timestamp: Buffer.from(Date.now().toString()),
-                        address: subaddress(rootAddress, messagesNum),
-                        payload: Buffer.from(data),
-                        pk,
-                        prev: undefined, // TODO add prev signature
-                        signSk: signKeyPair.sk
-                      }
+      const message = {
+        timestamp: Buffer.from(Date.now().toString()),
+        address: subaddress(rootAddress, messagesNum),
+        payload: Buffer.from(data),
+        pk,
+        prev: undefined, // TODO add prev signature
+        signSk: signKeyPair.sk
+      }
       relayClients.get(relay.toString('hex')).send(message)
-      messages.get(currentContact.alias).out.push(message) // TODO store in levelDB and append to chat
+      messages.get(currentContact.alias).out.push(message)
       await db.put(currentContact.alias + '!' + message.timestamp, data)
       gui.appendChatMessage(new Date(parseInt(message.timestamp.toString())).toISOString(), 'user', data)
     }
@@ -85,16 +97,15 @@ const getRelayClient = (relay) => {
 const getMessages = async (relayClient, invitation) => {
   const { rootAddress } = invitationTransform(invitation)
   const messages = []
-  for (var i = 0; true; i++) {
+  for (let i = 0; true; i++) {
     const message = await relayClient.db.get(Buffer.from(subaddress(rootAddress, i)))
     if (!message || !message.value) break
 
-    try{
-      messages.push(c.decode(query, message.value))
-    }catch(err){
+    try {
+      messages.push(decode(query, message.value))
+    } catch (err) {
       console.log(err)
     }
-
   }
   console.log(messages)
   return messages
@@ -104,13 +115,14 @@ gui.guiEmitter.on('contact-gui-update', async (contactIndex) => {
   const config = await loadConfig()
   const alias = config.contacts.filter(c => c.active)[contactIndex].alias
   const getDecryptedInbox = async (inbox) => {
+    console.log(inbox.length)
     const masterkey = Buffer.from(config.masterkey, 'hex')
     const masterKeyIndex = config.contacts.map(c => c.alias).indexOf(alias)
     const { keyPair } = await contact(masterkey, masterKeyIndex)
-    return inbox.map(m => ({...m, payload: crypto.decrypt(m.payload, keyPair.pk, keyPair.sk)}))
+    return inbox.map(m => ({ ...m, payload: crypto.decrypt(m.payload, keyPair.pk, keyPair.sk) }))
   }
   const inbox = await getDecryptedInbox(messages.get(alias).in)
-  const outbox =  messages.get(alias).out // already decrypted
+  const outbox = messages.get(alias).out // already decrypted
   gui.renderChat(alias, outbox, inbox)
 })
 
